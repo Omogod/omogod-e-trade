7274937a24712889a05e465496519e5dbcbb50bb
Omogod <godstimeomoregie0@gmail.com>
2023-01-15 11:35:01 +0100

    cleaned the code

--- mongodb-server-test/fixtures/index.ts
+++
@@ -1,8 +0,0 @@
-export const fakeUserData = {
-    name: 'Dummy',
-    email: 'dummy@user.com',
-    password: '********',
-    gender: 'student',
-    role: 'student',
-    phone: '1234567890'
-  };
